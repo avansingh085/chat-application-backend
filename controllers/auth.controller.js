047af0256a869c6a78fa041ba86caab67783f35c
Avan <avansingh085@gmail.com>
2026-03-11 03:36:32 +0530

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -33,8 +33,8 @@ const login = async (req, res) => {
 
 const signUp = async (req, res) => {
     try {
-        const { user, token } = await signUpUser(req.body);
-        res.status(201).json({ success: true, id: user._id, token });
+        const { user } = await signUpUser(req.body);
+        res.status(201).json({ success: true, });
     } catch (err) {
         res.status(err.status || 500).json({ success: false, message: err.message });
     }
@@ -51,20 +51,25 @@ const logout = (req, res) => {
     }
 };
 
+
 const checkToken = (req, res, next) => {
     try {
-       const token = req.headers.authorization;
-       
-        if (!token) return res.status(401).json({ message: "No token provided" });
+        const token = req.cookies.ChatsToken;
+
+        if (!token) {
+            return res.status(401).json({ message: "No token provided" });
+        }
 
         const decoded = verifyToken(token);
         req.user = decoded;
+
         next();
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(401).json({ message: "Invalid token" });
     }
 };
+
 
 module.exports = { login, signUp, logout, checkToken };
 
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,16 +1,21 @@
-const { verifyToken} = require("../utils/token.util");
+const { verifyToken } = require("../utils/token.util");
+
 const checkToken = (req, res, next) => {
     try {
-       const token = req.headers.authorization;
-        if (!token) return res.status(401).json({ message: "No token provided" });
+        const token = req.cookies.ChatsToken;
+
+        if (!token) {
+            return res.status(401).json({ message: "No token provided" });
+        }
 
         const decoded = verifyToken(token);
         req.user = decoded;
+
         next();
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(401).json({ message: "Invalid token" });
     }
 };
 
-module.exports=checkToken;
+module.exports = checkToken;
