97f42eb9f2f231601f982896b17a8ccc648b1559
Avan <avansingh085@gmail.com>
2026-04-18 21:52:08 +0530

    fix:import issue

--- config/aws-s3.config.js
+++ config/aws-s3.config.js
@@ -1,9 +1,6 @@
 
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-
-const app = express();
-
 const s3 = new S3Client({
     region: "ap-south-1",
     credentials: {
